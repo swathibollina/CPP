79d7516395040418c10d769dd23975ec6ebf91b9
swathibollina <115358999+swathibollina@users.noreply.github.com>
2026-05-30 16:56:41 +0530

    Create LevelOrdderTravesral.cpp

---
+++ LevelOrdderTravesral.cpp
@@ -0,0 +1,40 @@
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)
+        return ans;
+        queue<TreeNode* > q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> temp_ans;
+        while(!q.empty())
+        {
+            TreeNode* temp=q.front();
+            q.pop();
+            if(temp==NULL)
+            {
+                ans.push_back(temp_ans);
+                temp_ans.resize(0);
+                if(q.size()>0)
+                q.push(NULL);
+            }
+            else
+            {
+                temp_ans.push_back(temp->val);
+                if(temp->left!=NULL)
+                q.push(temp->left);
+                if(temp->right!=NULL)
+                q.push((temp->right));
+            }
+        }
+        return ans;
+    }
+};
