d8910d14331ad6ac2e6bb8877cc35cbb5e82e893
swathibollina <115358999+swathibollina@users.noreply.github.com>
2026-05-30 20:44:52 +0530

    Update LevelOrderTraversal.cpp

--- LevelOrderTraversal.cpp
+++ LevelOrderTraversal.cpp
@@ -5,6 +5,17 @@ Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[9,20],[15,7]]
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
