d6cdf0aeb8d55f30124e465e2d26058d0a179e8e
swathibollina <115358999+swathibollina@users.noreply.github.com>
2026-06-11 01:08:01 +0530

    Update ZigZagLevelOrderTraversal.cpp

--- ZigZagLevelOrderTraversal.cpp
+++ ZigZagLevelOrderTraversal.cpp
@@ -48,3 +48,49 @@ public:
         return ans;
     }
 };
+
+// USING TWO STACKS
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)
+        return ans;
+        stack<TreeNode* > s1;
+        stack<TreeNode* > s2;
+        s1.push(root);
+        while(!s1.empty() || !s2.empty())
+        {
+            vector<int> v;
+            while(!s1.empty())
+            {
+                TreeNode* temp=s1.top();
+                s1.pop();
+                v.push_back(temp->val);
+                if(temp->left!=NULL)
+                s2.push(temp->left);
+                if(temp->right!=NULL)
+                s2.push(temp->right);
+            }
+            if(v.size()!=0)
+            {
+                ans.push_back(v);
+                v.clear();
+            }
+            while(!s2.empty())
+            {
+                TreeNode* temp=s2.top();
+                s2.pop();
+                v.push_back(temp->val);
+                if(temp->right!=NULL)
+                s1.push(temp->right);
+                if(temp->left!=NULL)
+                s1.push(temp->left);
+            }
+            if(v.size()!=0)
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
